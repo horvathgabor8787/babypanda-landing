5b9ebd4a551eca6f0683b68c5a326e4fef200e8d
horvathgabor8787 <horvathgabor8787@gmail.com>
2025-09-07 22:57:18 +0200

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'happybabysss.myshoprenter.hu' },
+      { protocol: 'https', hostname: 'www.babypanda.hu' },
+      { protocol: 'https', hostname: 'babypanda.hu' },
+      { protocol: 'https', hostname: 'images.unsplash.com' }
+    ]
+  }
+};
+module.exports = nextConfig;
